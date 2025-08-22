9b5af79d4afc3d281b2aa433fbae65411a366ca1
Kartik Gupta <156841129+orignlkartik1@users.noreply.github.com>
2025-08-22 21:29:46 +0530

    Update Video.py

--- Video.py
+++ Video.py
@@ -3,6 +3,8 @@ import cv2 as cv
 # This function use for open camera. If you have more than two cameras you can pass argument 1,2,... . O is for your default camera.
 cap=cv.Videocapture(0) # you can also play any video passing file name in place of 0.
 
+four=cv.VideoWriter_fourcc(*'XVID')
+out=cv.VideoWriter("output.avi",four,20,(640,480))
 # use if for checking camera is open or not.
 if not cap.isOpened():
   print("Your camera is not working.")
@@ -18,7 +20,7 @@ while True:
   if not ret:
     print("Can't receive frame.")
     break
-
+  out.write(frame)
   # you have choice to convert your video stream in any color. Its convert your color
   gray=cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
 
@@ -28,6 +30,7 @@ while True:
   if cv.waitKey(1)==ord('q'):
       break
 cap.release() 
+out.release()
 cv.destroyAllwindows()
     
   
